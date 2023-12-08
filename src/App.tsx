500a3ea348285bf62f0446285a4b3fd142f0b8b3
aulanbekuulu <aulanbekuulu@srs.lan>
2023-12-08 12:03:29 +0600

    Обработка стандартного сценария. => Implementing without Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {useAppDispatch, useAppSelector} from "./store/hooks/redux";
 import {userSlice} from "./store/reducers/UserSlice";
 import {log} from "util";
+import {fetchUsers} from "./store/reducers/ActionCreators";
 
 function App() {
     const dispatch = useAppDispatch()
+    const {users,isLoading,error} = useAppSelector(state => state.userReducer)
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
   return (
     <div className="App">
+        {isLoading && <h1>Идет загрузка...</h1>}
+        {error && <h1>{error}</h1>}
+        {JSON.stringify(users, null, 2)}
     </div>
   );
 }
